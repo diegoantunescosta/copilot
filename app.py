12a1f6122a81c95827e7cb28c0bbca4621c59fab
Diego Costa <diego.costa@ciag.org.br>
2024-07-12 15:27:15 -0300

    feat: copilot

---
+++ app.py
@@ -0,0 +1,55 @@
+import json
+import requests
+
+
+model = "llama3" 
+
+def chat(messages):
+    r = requests.post(
+        "http://0.0.0.0:11434/api/chat",
+        json={"model": model, "messages": messages, "stream": True},
+        stream=True
+    )
+    r.raise_for_status()
+    output = ""
+
+    for line in r.iter_lines():
+        body = json.loads(line)
+        if "error" in body:
+            raise Exception(body["error"])
+        if body.get("done") is False:
+            message = body.get("message", "")
+            content = message.get("content", "")
+            output += content
+            print(content, end="", flush=True)
+
+        if body.get("done", False):
+            message["content"] = output
+            return message
+
+def main():
+    messages = [
+        {
+            "role": "system",
+            "content": (
+                "Você é um analista técnico da área de saúde que realiza análises com base no texto do requisito, "
+                "orientação e sugestão de evidência. Preciso que classifique a resposta como: Adequada, básica ou inadequada. "
+                "Apresente sugestões de melhorias com base no título do requisito, orientação e sugestão de evidência. "
+                "Seja direto e objetivo. Quero que faça uma análise no texto que eu te mandar. Sempre mencione os seguintes "
+                "tópicos: Visão, Missão, Valores."
+            )
+        }
+    ]
+
+    while True:
+        user_input = input("Enter a prompt: ")
+        if not user_input:
+            exit()
+        print()
+        messages.append({"role": "user", "content": user_input})
+        message = chat(messages)
+        messages.append(message)
+        print("\n\n")
+
+if __name__ == "__main__":
+    main()
